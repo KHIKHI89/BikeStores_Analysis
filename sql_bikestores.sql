3382058bf3a208b9967e11fc98b24f15397afd3f
Khadidja <111190473+KHIKHI89@users.noreply.github.com>
2023-09-22 21:16:54 +0100

    Add files via upload

---
+++ sql_bikestores.sql
@@ -0,0 +1,48 @@
+use BikeStores
+
+select * from production.brands
+select * from production.products
+select * from sales.orders
+
+
+select 
+  ord.order_id ,
+  CONCAT(cus.first_name ,' ' , cus.last_name) as customers,
+  cus.city,
+  cus.state,
+  ord.order_date,
+  Sum(ite.quantity) as 'total units',
+  sum(ite.quantity * ite.list_price) as 'revenue',
+  pro.product_name,
+  cat.category_name,
+  sto.store_name,
+  bra.brand_name,
+  concat(sta.first_name,' ',sta.last_name) as staffs
+
+
+from sales.orders ord 
+join sales.customers cus 
+on ord.customer_id = cus.customer_id
+join sales.order_items ite 
+on ord.order_id = ite.order_id
+join production.products pro
+on ite.product_id = pro.product_id
+join production.categories cat 
+on pro.category_id = cat.category_id
+join sales.stores sto
+on ord.store_id = sto.store_id
+join production.brands bra
+on pro.brand_id = bra.brand_id
+join sales.staffs sta
+on ord.staff_id = sta.staff_id
+group by 
+  ord.order_id ,
+  CONCAT(cus.first_name ,' ' , cus.last_name),
+  cus.city,
+  cus.state,
+  ord.order_date,
+  pro.product_name,
+  cat.category_name,
+  sto.store_name,
+  bra.brand_name,
+  concat(sta.first_name,' ',sta.last_name)
